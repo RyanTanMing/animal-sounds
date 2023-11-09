fa5bae31bfec697fe8499d0b3de5af287b35639f
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2023-11-09 16:23:24 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,40 @@ classifier.classify(got_results)
  }
  function got_results(error,results) {
     console.log('got_results')
+    if (error) {
+        console.log(error)
+
+    }
+    else {
+        console.log(results)
+        r=Math.floor(Math.random()*255)+1
+        g=Math.floor(Math.random()*255)+1
+        b=Math.floor(Math.random()*255)+1
+        document.getElementById("result_label").innerHTML='I can hear - '+results[0].label
+        document.getElementById("result_confidence").innerHTML='Accuracy - '+(results[0].confidence*100).toFixed(2)+"%"
+        document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")"
+        document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")"
+        img1=document.getElementById("Alien_1")
+        
+        if (results[0].label=="barking") {
+            img1.src="dog.gif"
+            
+        }
+        else if (results[0].label=="mooing") {
+            img1.src="cow.gif"
+            
+        }
+        else if (results[0].label=="meowing") {
+            img1.src="cat.gif"
+           
+        }
+        else if (results[0].label=="roaring") {
+            img1.src="lion-roar.gif"
+           
+        }
+        else {
+            img1.src="ear.jpg"
+         
+        }
+    }
  }
